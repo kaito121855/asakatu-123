7db845755a13b6c43a32cec56c83dbf407725166
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-24 15:27:30 +0900

    new・createアクションの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,22 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:user_id)
+  end
 end
